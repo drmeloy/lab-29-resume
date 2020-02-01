4907684f2707fd905d74bba49c35344a74c4a631
drmeloy <drmeloy@gmail.com>
2020-02-01 16:56:47 -0700

    wrote state and props

--- src/containers/Resume.jsx
+++ src/containers/Resume.jsx
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 
 export default class Resume extends Component {
-  state: {
-
+  state = {
+    name: '',
+    title: '',
+    summary: '',
+    phone: '',
+    email: '',
+    sites: [],
+    skills: [],
+    experience: [],
+    projects: [],
+    education: []
   }
 
   render() {
+    const { name, title, summary, phone, email, sites, skills, experience, projects, education } = this.state;
+
     return (
       <>
-        <Name />
-        <Title />
-        <Summary />
-        <Contact />
-        <Skills />
-        <Experience />
-        <Projects />
-        <Education />
+        <Name name={name} />
+        <Title title={title} />
+        <Summary summary={summary} />
+        <Contact phone={phone} email={email} sites={sites} />
+        <Skills skills={skills} />
+        <Experience experience={experience} />
+        <Projects projects={projects} />
+        <Education education={education} />
       </>
     );
   }
